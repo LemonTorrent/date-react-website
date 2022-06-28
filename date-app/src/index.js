fdb80bd18b00b72a93d14b07f6146dabef98fba4
LemonTorrent <nyarbrough14@gmail.com>
2022-06-27 19:31:33 -0700

    Setting up basic table in react

--- date-app/src/index.js
+++ date-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Checklist from './CheckList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Checklist />
     <App />
   </React.StrictMode>
 );
